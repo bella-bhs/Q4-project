8adef35c6b5780a07ee09bf63805ee42b9e36917
bella-bhs <abakhshi25@belmontschools.net>
2025-04-14 09:59:37 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+function setup() {
+  createCanvas(800, 400,);
+}
+
+function draw() {
+  background('#0082cb');
+  
+  strokeWeight(0);
+  fill('white');
+  quad(350,0,
+       420,20,
+       420,370,
+       350,400,);
+  
+       
+  
+  stroke(255);
+  strokeWeight(1);
+fill('white');
+  
+  fill('rgb(138,253,104)');
+  circle(390,80, 80);
+  
+  noFill();
+  circle(370,75,12)
+  circle(410,70,12)
+  circle(400,100,12)
+  circle(382,50,12)
+ 
+  
+  
+}
