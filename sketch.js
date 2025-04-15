e6b02ea03878a41b7fd9cb60079a30775d6955f7
bella-bhs <abakhshi25@belmontschools.net>
2025-04-15 10:12:16 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,46 @@
-
+let x; 
+let y;
+let player1Score = 0;
+let player2Score = 0;
 function setup() {
   createCanvas(800, 400,);
+  x =390
+  y = 80
 }
 
 function draw() {
   background('#0082cb');
   
-  strokeWeight(0);
+ 
+  textSize(20);
+  
+  text("player 1: " +player1Score, 10, 20);
+  text("player 2: " + player2Score,400,20)
+  
+ 
   fill('white');
-  quad(350,0,
-       420,20,
-       420,370,
-       350,400,);
+  line(385,0,388,400)
   
        
   
   stroke(255);
   strokeWeight(1);
-fill('white');
+
   
   fill('rgb(138,253,104)');
-  circle(390,80, 80);
+  circle(x,y, 80);
+  
+  fill('black')
+  rect(1,150,40,150)
+
+  rect(758,100,40,150)
   
   noFill();
-  circle(370,75,12)
-  circle(410,70,12)
-  circle(400,100,12)
-  circle(382,50,12)
+  circle(x-20,y-5,13)
+  circle(x+20,y-10,13)
+  circle(x+10,y+20,13)
+  circle(x-7,y-30,13)
+ 
  
   
   
